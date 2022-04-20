c2111b9939f48c52ef1c9b743ed0d35245557001
Sorrawit <sorrawit456789@gmail.com>
2022-04-20 14:01:02 +0700

    add another return

--- createJSONCL.js
+++ createJSONCL.js
@@ -21,4 +21,23 @@ function createJSONCL(className, isHW) {
             ]
         });
     }
+    return ({
+        "type": "box",
+        "layout": "vertical",
+        "spacing": "md",
+        "contents": [
+            {
+                "type": "text",
+                "text": className,
+                "weight": "bold",
+                "align": "start",
+                "contents": []
+            },
+            {
+                "type": "separator",
+                "margin": "lg",
+                "color": "#000000FF"
+            }
+        ]
+    });
 }

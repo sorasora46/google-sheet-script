f3a1b9a9eb0ac44c2455e26b2702b6846e493e16
Sorrawit <sorrawit456789@gmail.com>
2022-04-20 12:08:30 +0700

    change to box

--- createJSONCT.js
+++ createJSONCT.js
@@ -1,6 +1,26 @@
 function createJSONCT(name, contact) {
   return ({
-    "type": "text",
-    "text": name,
+    "type": "box",
+    "layout": "vertical",
+    "contents": [
+      {
+        "type": "text",
+        "text": "Mr. Sorrawit Kwanja",
+        "weight": "bold",
+        "align": "start",
+        "contents": []
+      },
+      {
+        "type": "text",
+        "text": "sorrawit.kwan@kmutt.ac.th",
+        "align": "start",
+        "contents": []
+      },
+      {
+        "type": "separator",
+        "margin": "lg",
+        "color": "#000000FF"
+      }
+    ]
   });
 }
